b9de615d194899fee808ab29005b86b3f39d6e80
simranpandita0421 <147545858+simranpandita0421@users.noreply.github.com>
2023-10-11 11:17:57 +0530

    Update ADS_LabAss3.c

--- ADS_LabAss3.c
+++ ADS_LabAss3.c
@@ -1,28 +1,30 @@
 /*meena loves sweets and wants the sweetness of some cookies to be greater than value K. To do this, 2 cookies with the least sweetness are repeatedly mixed this creates a special combined cookie with sweetness = (first least sweet cookie + second least sweet cookie) use link list to maintain all the cookies and add newly created cookies and the end of link list.*/
 #include <stdio.h>
-void mCookies(int cookies[], int size, int k) {
-    while(cookies[0] < k) 
+void mC(int c[], int s, int k)
+ {
+    while(c[0] < k) 
 {
-        int leastsweet = cookies[0];
-        int secondleastsweet =cookies[1];    
+        int leastsweet = c[0];
+        int secondleastsweet =c[1];    
         int combinedsweetness = leastsweet + secondleastsweet;       
       
     for(int i = 1; i < s-1; i++) {
-            cookies[i] = cookies[i+1];
+            c[i] = c[i+1];
         }      
      
-       cookies[s-2] = combinedsweetness;
+       c[s-2] = combinedsweetness;
         s--;
     }
 }
 int main() {
-    int cookies[] = {1, 3, 5, 9, 10, 15};
+    int c[] = {1, 3, 5, 9, 10, 15};
     int k = 10;
-    int s = sizeof(cookies) / sizeof(cookies[0]);  
-    mCookies(cookies, s, k);    
+    int s = sizeof(c) / sizeof(c[0]);  
+    mC(c, s, k);    
     printf("Cookies with sweetness greater than %d: ", k);
-    for(int i = 0; i < s; i++) {
-        printf("%d ", cookies[i]);
+    for(int i = 0; i < s; i++) 
+{
+        printf("%d ", c[i]);
     }  
     return 0;
 }
